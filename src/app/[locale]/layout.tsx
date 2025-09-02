159ac53ce61d6910e718c8c61ab1c364c91b5092
Umar Djuraev <umardjuraevcode@gmail.com>
2025-09-02 16:52:38 +0500

    yandex metrix added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -54,6 +54,23 @@ export default async function RootLayout({
             gtag('config', 'G-6J223825M8');
           `}
         </Script>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103992666', 'ym');
+
+            ym(103992666, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103992666" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <ProgressBarClient />
         <I18nProvider initialLocale={param.locale}>
           <MainLayout>{children}</MainLayout>
